6b879454e144dc92b3296ed0abb39bc93b5f7a35
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 17:49:39 +0100

    copy prev state into reducer before modify

--- src/redux/icecream/icecreamReducer.js
+++ src/redux/icecream/icecreamReducer.js
@@ -9,6 +9,7 @@ const icecreamReducer = (state = initialState, action) => {
     switch (action.type) {
         case BUY_ICECREAM:
             return {
+                ...state,
                 numOfIcecream: state.numOfIcecream - 1
             }
         default:
