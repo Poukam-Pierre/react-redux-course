f3fb79e1d1b4cf2c660199b77ae23871b6acdf56
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 19:30:22 +0100

    arrange container according to an other add parameter

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { buyCake } from '../redux';
 import { connect } from 'react-redux'
 
 
 function CakeContainer(props) {
+    const [number, setNumber] = useState(1)
     return (
         <div>
             <h2>Number of cake - {props.numOfCakes}</h2>
-            <button onClick={props.buyCake}>Buy Cake</button>
+            <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} />
+            <button onClick={() => props.buyCake(number)}>Buy {number} Cake</button>
         </div>
     );
 }
@@ -20,7 +22,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        buyCake: () => dispatch(buyCake())
+        buyCake: (number) => dispatch(buyCake(number))
     }
 }
 
