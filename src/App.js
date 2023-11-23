0f7f3687f1c7355f36ff98198c7300c73b5428df
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 16:58:36 +0100

    creating hook component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import HookCakeContainer from './components/hookCakeConatiner';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        <HookCakeContainer />
       </div>
     </Provider>
   );
