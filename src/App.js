d8c4acd2dbb64b16ed6f89a0d1202cb0e9bbe390
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 17:34:09 +0100

    show icecream container to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import CakeContainer from './components/CakeContainer';
+// import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux'
 import store from './redux/store';
+import IcecreamContainer from './components/IcecreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <CakeContainer />
+        <IcecreamContainer />
       </div>
     </Provider>
   );
