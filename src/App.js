0e6ae57ed653976fe4fc2543886071e9fbd4f7e4
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 17:16:15 +0100

    show icecram component using hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './redux/store';
-import HookCakeContainer from './components/hookCakeConatiner';
+// import HookCakeContainer from './components/hookCakeConatiner';
+import HookIcecreamContainer from './components/hookIcecreamContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <HookCakeContainer />
+        <HookIcecreamContainer />
       </div>
     </Provider>
   );
