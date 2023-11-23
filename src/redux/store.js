d01d198ef4c9e9f6acdec553442a6bf9b314f57e
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 17:48:53 +0100

    consider rootReducer to store buy createStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { createStore } from 'redux'
 // import cakeReducer from './cake/cakeReducer'
-import icecreamReducer from './icecream/icecreamReducer'
+// import icecreamReducer from './icecream/icecreamReducer'
+import rootReducer from './rootReducer'
 
-const store = createStore(icecreamReducer)
+const store = createStore(rootReducer)
 
 export default store
