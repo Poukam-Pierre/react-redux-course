1bef517c8ba5508c6846b44c82c3e57ade72a3f3
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-23 17:16:36 +0100

    adddin icecreamREducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,7 @@
 import { createStore } from 'redux'
 import cakeReducer from './cake/cakeReducer'
+import icecreamReducer from './icecream/icecreamReducer'
 
-const store = createStore(cakeReducer)
+const store = createStore(icecreamReducer)
 
 export default store
