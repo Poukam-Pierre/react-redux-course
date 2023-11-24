8cc9cac3d161235ab112bb8875413915326eecbb
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-24 19:07:32 +0100

    adding persistGate on the top of project

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { Provider } from 'react-redux'
 import UserContainer from './container/UserContainer';
+import { persistor } from './redux/store'
 import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react';
+
+
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <UserContainer />
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="App">
+          <UserContainer />
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
