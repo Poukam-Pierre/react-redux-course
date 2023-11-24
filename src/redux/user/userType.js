dc2e91fcfc8f65ca98b2755c70ec8e45adf49581
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-24 13:16:07 +0100

    set up general types for various

--- src/redux/user/userType.js
+++ src/redux/user/userType.js
@@ -1,3 +1,10 @@
 export const FETCH_USERS_REQUEST = 'FETCH_USERS_REQUEST';
 export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_FAILURE = 'FETCH_USERS_FAILURE';
+
+// General types for the various
+export const API = "API";
+export const API_START = "API_START";
+export const API_END = "API_END";
+export const ACCESS_DENIED = "ACCESS_DENIED";
+export const API_ERROR = "API_ERROR";
