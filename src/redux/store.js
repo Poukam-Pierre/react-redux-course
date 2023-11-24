18b45a211c769caa9ce057f291315142b05f597c
Poukam-Pierre <ngamaleu2011@gmail.com>
2023-11-24 19:07:53 +0100

    update store for persistance

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from "redux";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit"
 import reducer from "./user/userReducer";
 import apiMiddleware from "../middleware/api";
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist'
 
-const store = createStore(reducer, applyMiddleware(apiMiddleware))
+
+const persitConfig = {
+    key: 'root',
+    storage
+}
+
+const persisterReduced = persistReducer(persitConfig, reducer)
+const store = configureStore({ reducer: persisterReduced, middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiMiddleware) })
+// const store = createStore(reducer, applyMiddleware(apiMiddleware))
+export const persistor = persistStore(store)
 
 export default store;
